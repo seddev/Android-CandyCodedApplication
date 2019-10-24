696ad270b35ae19318f3394fb44a215c36e79e84
seddev <55341131+seddev@users.noreply.github.com>
2019-10-25 00:15:52 +0300

    4

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -2,9 +2,8 @@ package com.pluralsight.candycoded;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.squareup.picasso.Picasso;
 
--- app/src/test/java/com/pluralsight/candycoded/_1_StartTheStoreInformationActivity.java
+++ app/src/test/java/com/pluralsight/candycoded/_1_StartTheStoreInformationActivity.java
@@ -2,9 +2,8 @@ package com.pluralsight.candycoded;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
